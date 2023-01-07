2ad9c51aa0b95b0354bba87ee6d1751b7bda6af4
TerebinovSergey <neznacomec94@gmail.com>
2023-01-07 23:52:48 +0400

    fix: delete Rest_button from main-page.ts

--- src/pages/home/main-page.ts
+++ src/pages/home/main-page.ts
@@ -5,7 +5,7 @@ import { ProductCard } from '../../conponents/product-card/ProductCard';
 import { QueryParameters, FilteredProducts } from '../../conponents/queryParameters/QueryParameters';
 import { getElement } from '../../conponents/general/general';
 import { Footer } from '../../conponents/footer/footer';
-import CounterComponent, { Rest_button } from '../../conponents/counter/Counter';
+import CounterComponent from '../../conponents/counter/Counter';
 export class MainPage {
   groupCategory: FilterGroup;
   groupBrand: FilterGroup;
